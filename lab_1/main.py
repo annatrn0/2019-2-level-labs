ce8017cd780c3a9063d05cb2a2caba42e1e98446
annatrn0@gmail.com <annatrn0@gmail.com>
2019-10-07 11:59:30 +0300

    changed 1st and 2nd functions

--- lab_1/main.py
+++ lab_1/main.py
@@ -5,16 +5,36 @@ Count frequencies dictionary by the given arbitrary text
 
 
 def calculate_frequences(text: str) -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    ('word:10')
+    if isinstance(text, str):
+        text = "It is8 w&hat it( is"
+        new_text = " "
+        stop_marks = ['.', ',', ':', '"', '`', '[', ']', '?', '!', '@',
+                      '&', "'", '-', '$', '^', '*', '(', ')', '_', '“',
+                      '”', '’', '#', '%', '<', '>', '*', '~', '0', '1',
+                      '2', '3', '4', '5', '6', '7', '8', '9', '\n']
+        for item in text:
+            if not item in stop_marks:
+                new_text += item
+        new_text = new_text.lower().split()
+        # print(sep_text)
+
+        frequency_word = {}
+        for i in new_text:
+            count = frequency_word.get(i, 0)
+            frequency_word[i] = count + 1
+            return (frequency_word)
+
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
-    pass
+    dictionary_stop = {}
+    dictionary_stop = frequency_word.copy()
+    stop_words = ['it', 'what']
+    if dictionary_stop is not None and stop_words is not None:
+        for k in stop_words:
+            if k in dictionary_stop.keys():
+                del dictionary_stop[k]
+                return (dictionary_stop)
+
 
 def get_top_n(frequencies: dict, top_n: int) -> tuple:
     """
