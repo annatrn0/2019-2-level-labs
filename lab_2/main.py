b1bcc84c22f0d4db7798330549d72f1c04b7198c
annatrn0@gmail.com <annatrn0@gmail.com>
2019-11-26 15:19:17 +0300

    1st, 2nd, 3rd finished

--- lab_2/main.py
+++ lab_2/main.py
@@ -12,11 +12,23 @@ def generate_edit_matrix(num_rows: int, num_cols: int) -> list:
     while num_rows > 0:
         matrix.append(column)
         num_rows = num_rows - 1
-        return(matrix)
+        return (matrix)
 
 
 def initialize_edit_matrix(edit_matrix: tuple, add_weight: int, remove_weight: int) -> list:
-    pass
+    def initialize_edit_matrix(edit_matrix: tuple, remove_weight, add_weight):
+        i = 1
+        j = 1
+        for elem in range(len(edit_matrix)):
+            for thing in range(len(edit_matrix[0])):
+                while i < len(edit_matrix) and j < len(edit_matrix[i]):
+                    edit_matrix[0][0] = 0
+                    edit_matrix[i][0] = edit_matrix[i - 1][0] + remove_weight
+                    i += 1
+                    edit_matrix[0][j] = edit_matrix[0][j - 1] + add_weight
+                    j += 1
+                    return (edit_matrix)
+
 
 
 def minimum_value(numbers: tuple) -> int:
@@ -27,7 +39,7 @@ def minimum_value(numbers: tuple) -> int:
             if numbers[i] > numbers[i + 1]:
                 min = numbers[i + 1]
         i += 1
-        return(min)
+        return (min)
 
 
 
